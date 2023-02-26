a5906e6d0c93c65ccb7b7f59b83c7694344bee8f
Vsevolod <vsevolodehe@mail.ru>
2023-02-27 00:58:06 +0500

    did something

--- app.js
+++ app.js
@@ -3,15 +3,28 @@ import express from 'express'
 import path from 'path'
 import fs from 'fs'
 import AdmZip from "adm-zip"
-import convert from 'xml-js'
+import xml2js from 'xml2js';
+import convert from 'xml-js';
 
+import encoding  from 'encoding';
+
+import iconv from 'iconv-lite';
+
+
+
+
+
+
+
+const parser = new xml2js.Parser({ attrkey: "ATTR" });
 const app = express()
 const port = 3000
 app.use(express.json())
 
+let bicPath = path.resolve() +  '/src/20230226ED01OSBR.zip'
 
 app.get('*', (req, res) => {
-	let bicPath = path.resolve() +  '/src/20230226ED01OSBR.zip'
+	
 	const fileStream = fs.createReadStream(bicPath);
   fileStream.pipe(res);
 	res.status(200)
@@ -23,14 +36,28 @@ app.listen(port, () => {
 })
 
 async function main(){
-	await downloadFile('http://localhost:' + port, path.resolve() + `/saved/BIC.zip`)
+	await downloadFile('http://localhost:3000', path.resolve() + `/saved/BIC.zip`)
 	await unzip()
+
+	let jsonResult
+	let xml_string = fs.readFileSync(path.resolve() + '/unzipped/20230226_ED807_full.xml', 'utf-8');
+	
+	parser.parseString(xml_string, function(error, result) {
+    if(error === null) {
+			console.log('Got base');
+			jsonResult = result
+
+    }
+    else {
+      console.log(error);
+    }
+});
+
+	let parsingResultForBase = parseForBase(jsonResult.ED807.BICDirectoryEntry)	
 }
 
 const downloadFile = (async (url, path) => {
   const res = await fetch(url);
-	console.log(res.status)
-	
 	const fileStream = fs.createWriteStream(path);
   await new Promise((resolve, reject) => {
       res.body.pipe(fileStream);
@@ -44,4 +71,38 @@ async function unzip(){
 	var zip = new AdmZip(path.resolve() + `/saved/BIC.zip`)
 	var zipEntries = zip.getEntries()
 	zip.extractAllTo(path.resolve() + '/unzipped', /*overwrite*/ true)
+}
+
+
+function parseForBase(data){
+
+	console.log(data[0].ParticipantInfo)
+	let res = [
+	// {bic:123, name: 'банк1', corrAccount: '001'},
+  ]
+
+	let i = true
+
+	// data.forEach(e => {
+
+		// if (i){
+		// 	console.log(e.ParticipantInfo)
+			
+		// }
+		// i = false
+	// 	res.push({
+	// 		bic: e.ATTR.BIC,
+	// 		name: e.ParticipantInfo[0].ATTR.NameP,
+	// 		corrAccount: '123'
+
+	// 	})		
+	// });
+
+
+	// console.log(res[0])
+
+
+	// another parser which also doesnt support kirilica
+	// console.log(convert.xml2json( fs.readFileSync(path.resolve() + '/unzipped/20230226_ED807_full.xml') ,{compact: false, spaces: 4}))
+	return res
 }
